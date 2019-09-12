2e2717bd6200d13725018e9de185abad799e4b07
roxanariza <roxana_r2000@yahoo.com>
2019-09-12 15:54:27 +0300

    Add importOnlyActiveAccessLevel and list id in config file

--- config/maropost.php
+++ config/maropost.php
@@ -4,4 +4,6 @@ return [
     'auth_token' => env('MAROPOST_API_KEY', 'key'),
     'account_id' => env('MAROPOST_ACCOUNT_ID', 123),
     'base_api_url' => 'http://api.maropost.com/accounts/',
+    'importOnlyActiveAccessLevel' => false,
+    'listId' => 31
 ];
